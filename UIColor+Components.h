226670d58501550c0f2e16b6096ce33dd8f839ec
David Keegan <git@davidkeegan.com>
2013-09-17 23:01:04 -0700

    initial commit

---
+++ UIColor+Components.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+Components.h
+//  UIColor+Components
+//
+//  Created by David Keegan on 9/15/13.
+//  Copyright (c) 2013 1kLabs, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor(Components)
+
+- (CGFloat)alphaComponent;
+- (NSArray *)componentArray;
+
+@end
---
+++ UIColor+Components.m
@@ -0,0 +1,35 @@
+
+//
+//  UIColor+Components.m
+//  UIColor+Components
+//
+//  Created by David Keegan on 9/15/13.
+//  Copyright (c) 2013 1kLabs, Inc. All rights reserved.
+//
+
+#import "UIColor+Components.h"
+
+@implementation UIColor(Components)
+
+- (CGFloat)alphaComponent{
+    return CGColorGetAlpha([self CGColor]);
+}
+
+- (NSArray *)componentArray{
+    CGFloat red, green, blue, alpha;
+    const CGFloat *components = CGColorGetComponents([self CGColor]);
+    if(CGColorGetNumberOfComponents([self CGColor]) == 2){
+        red = components[0];
+        green = components[0];
+        blue = components[0];
+        alpha = components[1];
+    }else{
+        red = components[0];
+        green = components[1];
+        blue = components[2];
+        alpha = components[3];
+    }
+    return @[@(red), @(green), @(blue), @(alpha)];
+}
+
+@end
