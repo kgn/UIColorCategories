171d4c0539fe4c505531a25bf733c8ec723695e3
David Keegan <git@davidkeegan.com>
2013-09-17 23:01:46 -0700

    Update UIColor+Components.h

--- UIColor+Components.h
+++ UIColor+Components.h
@@ -3,7 +3,7 @@
 //  UIColor+Components
 //
 //  Created by David Keegan on 9/15/13.
-//  Copyright (c) 2013 1kLabs, Inc. All rights reserved.
+//  Copyright (c) 2013 David Keegan All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
